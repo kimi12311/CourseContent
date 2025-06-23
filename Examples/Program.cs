d9c413349ac25a9d4f8d965e0aea618d387ef520
Kimi A <kimianderssonmusic@gmail.com>
2025-06-23 18:45:37 +0200

    Pretty colors

---
+++ Examples/Program.cs
@@ -0,0 +1,29 @@
+﻿using Raylib_cs;
+
+namespace Examples;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Pink);
+            for (var i = 0; i < Raylib.GetScreenWidth(); i += 50)
+            {
+                Raylib.DrawLine(i,0, (int)(i * (Math.Sin(Raylib.GetTime()) * 10)), Raylib.GetScreenHeight(), Color.White);
+            }
+            for (var i = 0; i < Raylib.GetScreenHeight(); i += 50)
+            {
+                Raylib.DrawLine(0,i, 
+                    Raylib.GetScreenWidth(), (int)(i * (Math.Sin(Raylib.GetTime()) * 10)), Color.White);
+            }
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
